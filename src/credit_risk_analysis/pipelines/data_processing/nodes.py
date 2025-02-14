8403e6ce783be275d8fe8a288e1ad536de1d9f3d
gduartels <guiduartelsouza@gmail.com>
2025-02-14 20:49:29 -0300

    added node to join data

--- src/credit_risk_analysis/pipelines/data_processing/nodes.py
+++ src/credit_risk_analysis/pipelines/data_processing/nodes.py
@@ -1,68 +1,25 @@
 import pandas as pd
 
 
-def _is_true(x: pd.Series) -> pd.Series:
-    return x == "t"
-
-
-def _parse_percentage(x: pd.Series) -> pd.Series:
-    x = x.str.replace("%", "")
-    x = x.astype(float) / 100
-    return x
-
-
-def _parse_money(x: pd.Series) -> pd.Series:
-    x = x.str.replace("$", "").str.replace(",", "")
-    x = x.astype(float)
-    return x
-
-
-def preprocess_companies(companies: pd.DataFrame) -> pd.DataFrame:
-    """Preprocesses the data for companies.
-
-    Args:
-        companies: Raw data.
-    Returns:
-        Preprocessed data, with `company_rating` converted to a float and
-        `iata_approved` converted to boolean.
-    """
-    companies["iata_approved"] = _is_true(companies["iata_approved"])
-    companies["company_rating"] = _parse_percentage(companies["company_rating"])
-    return companies
-
-
-def preprocess_shuttles(shuttles: pd.DataFrame) -> pd.DataFrame:
-    """Preprocesses the data for shuttles.
-
-    Args:
-        shuttles: Raw data.
-    Returns:
-        Preprocessed data, with `price` converted to a float and `d_check_complete`,
-        `moon_clearance_complete` converted to boolean.
-    """
-    shuttles["d_check_complete"] = _is_true(shuttles["d_check_complete"])
-    shuttles["moon_clearance_complete"] = _is_true(shuttles["moon_clearance_complete"])
-    shuttles["price"] = _parse_money(shuttles["price"])
-    return shuttles
-
-
-def create_model_input_table(
-    shuttles: pd.DataFrame, companies: pd.DataFrame, reviews: pd.DataFrame
+def join_data(
+    base_pagamentos: pd.DataFrame,
+    base_cadastral: pd.DataFrame,
+    base_info: pd.DataFrame
 ) -> pd.DataFrame:
-    """Combines all data to create a model input table.
+    """Junta as bases de dados em uma única tabela.
 
     Args:
-        shuttles: Preprocessed data for shuttles.
-        companies: Preprocessed data for companies.
-        reviews: Raw data for reviews.
+        base_pagamentos: Base bruta com informações sobre transações (empréstimos) passados
+        base_cadastral: Base contendo informações cadastrais dos clientes
+        base_info: Base com informações adicionais dos clientes
     Returns:
-        Model input table.
-
+        df_completo: Tabela unindo todas as informações separadas
     """
-    rated_shuttles = shuttles.merge(reviews, left_on="id", right_on="shuttle_id")
-    rated_shuttles = rated_shuttles.drop("id", axis=1)
-    model_input_table = rated_shuttles.merge(
-        companies, left_on="company_id", right_on="id"
+
+    df_completo = (
+        base_pagamentos
+        .merge(base_cadastral, on=['ID_CLIENTE'], how='left')
+        .merge(base_info, on=['ID_CLIENTE', 'SAFRA_REF'], how='left')
     )
-    model_input_table = model_input_table.dropna()
-    return model_input_table
+
+    return df_completo
--- src/credit_risk_analysis/pipelines/data_processing/pipeline.py
+++ src/credit_risk_analysis/pipelines/data_processing/pipeline.py
@@ -1,28 +1,16 @@
 from kedro.pipeline import Pipeline, node, pipeline
 
-from .nodes import create_model_input_table, preprocess_companies, preprocess_shuttles
+from .nodes import join_data
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
             node(
-                func=preprocess_companies,
-                inputs="companies",
-                outputs="preprocessed_companies",
-                name="preprocess_companies_node",
-            ),
-            node(
-                func=preprocess_shuttles,
-                inputs="shuttles",
-                outputs="preprocessed_shuttles",
-                name="preprocess_shuttles_node",
-            ),
-            node(
-                func=create_model_input_table,
-                inputs=["preprocessed_shuttles", "preprocessed_companies", "reviews"],
-                outputs="model_input_table",
-                name="create_model_input_table_node",
+                func=join_data,
+                inputs=["pagamentos_desenvolvimento", "cadastral", "info"],
+                outputs="raw_data",
+                name="join_data_node",
             ),
         ]
     )
